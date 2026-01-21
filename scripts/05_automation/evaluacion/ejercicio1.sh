98473b4d9b35c2e258cd83fc50c1ee71fad6efed
J. Ignacio M <jmonsar029@g.educaand.es>
2026-01-21 19:59:42 +0100

    Corregidos algunos errores para encontrar discos

--- scripts/05_automation/evaluacion/ejercicio1.sh
+++ scripts/05_automation/evaluacion/ejercicio1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # almacena los discos que estan en el sistema en un fichero 
-df -x tmpfs -x devtmpfs --output=source | tail -n +2 | tr ' ' '\n' > fichero.txt
+df -x tmpfs -x devtmpfs --output=source,target | tail -n +2 | sed 's/ \//,\//' > fichero.txt
 
 # lee del fichero los nombres de los discos y comprueba 
 # el porcentaje del disco usado. Por cada disco que 
 # supere el 90% de capacidad lleno enviara un mensaje al 
 # administrador del sistema medinte el comando mail.
-while read nombre; do
-    porcentaje=$(df --output=pcent $nombre | tail -n +2 | sed 's/^  //' | cut -d'%' -f1)
+while IFS=',' read nombre montaje; do
+    porcentaje=$(df --output=pcent $montaje | tail -n +2 | sed 's/^  //' | cut -d'%' -f1)
     if [[ "$porcentaje" -gt 7 ]]; then
         echo "El disco $nombre se encuentra por encima del 90% de su almacenamiento." >> mensaje.txt
     fi
@@ -18,7 +18,7 @@ done < fichero.txt
 if [[ (-f mensaje.txt) && (-s mensaje.txt) ]];then
     # llama al archivo y lo redirecciona al cuerpo del mensaje de comando mail, el 
     # cual es enviado al usuario root. La informacion se almacena en /var/mail/root
-    cat mensaje.txt | mail -s "Almacenamiento del sistema" hostmaster@localhost
+    cat mensaje.txt | mail -s "Almacenamiento del sistema" root@localhost
     rm mensaje.txt
 fi
 
